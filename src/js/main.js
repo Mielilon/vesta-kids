f2a148034c001f244f65fd73e517f9d4572495f8
--show <san4es021@gmail.com>
2020-12-10 20:41:20 +0300

    Добавлен параллакс в шщапку

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,17 @@
 AOS.init();
 
 //parallax
+document.body.addEventListener("mousemove", e => {
+  const maxOffset = 100
+  const offsetRatio = 15
+  const wrapper = $('#parallaxWrapper')[0]
+  const wrapperRect = wrapper.getBoundingClientRect()
+  const offsetX = (-e.clientX + wrapperRect.left + wrapperRect.width / 2) / offsetRatio
+  const offsetY = (-e.clientY + wrapperRect.top + wrapperRect.height / 2) / offsetRatio
+  const resultedX = Math.max(Math.min(offsetX, maxOffset), -maxOffset)
+  const resultedY = Math.max(Math.min(offsetY, maxOffset), -maxOffset)
+
+  $('#bedImg').css('transform', `translate(${resultedX}px, ${resultedY}px)`)
+  $('#figureLightYellow').css('transform', `translate(${resultedX * 1.5}px, ${resultedY * 1.5}px)`)
+  $('#figureYellow').css('transform', `translate(${resultedX * 0.5}px, ${resultedY * 0.5}px)`)
+});
